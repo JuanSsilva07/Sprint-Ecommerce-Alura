041b4be3b439312242c77e73eeb58daa4a0208cb
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-11-07 15:08:41 -0300

    att project

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,22 @@ formularioRodape.addEventListener("submit", (e) => {
   txtName.value = '';
   txtMensagem.value = '';
 })
+
+const inputs = document.querySelectorAll("input")
+
+inputs.forEach((input) => {
+  if (input.dataset.tipo === "preco") {
+    SimpleMaskMoney.setMask(input, {
+      prefix: "R$ ",
+      fixed: true,
+      fractionDigits: 2,
+      decimalSeparator: ",",
+      thousandsSeparator: ".",
+      cursor: "end",
+    })
+  }
+
+  input.addEventListener("blur", (evento) => {
+    valida(evento.target)
+  })
+})
