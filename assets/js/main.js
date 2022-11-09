d16d7ebfa878beffa294fa0dc9adcc91f8c6baeb
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-11-09 11:22:47 -0300

    add more js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,44 @@
-const formularioRodape = document.querySelector("#formEnvio");
+/* Reitrando a configuração de envio de Form e retirando os valores. */
+
+const formularioRodape = document.querySelector(".rodape__contato-form");
 formularioRodape.addEventListener("submit", (e) => {
-  e.preventDefault()
-  console.log("Deu certo")
-  txtName.value = '';
-  txtMensagem.value = '';
-})
+  e.preventDefault();
+  console.log("Deu certo");
+  txtName.value = "";
+  txtMensagem.value = "";
+});
 
-const inputs = document.querySelectorAll("input")
+/* Aplicando máscara para inserir o valor corretamente no campo de inserção. */
 
-inputs.forEach((input) => {
-  if (input.dataset.tipo === "preco") {
-    SimpleMaskMoney.setMask(input, {
-      prefix: "R$ ",
-      fixed: true,
-      fractionDigits: 2,
-      decimalSeparator: ",",
-      thousandsSeparator: ".",
-      cursor: "end",
-    })
-  }
+const args = {
+  afterFormat(e) {
+    console.log("afterFormat", e);
+  },
+  allowNegative: false,
+  beforeFormat(e) {
+    console.log("beforeFormat", e);
+  },
+  negativeSignAfter: false,
+  prefix: "R$ ",
+  suffix: "",
+  fixed: true,
+  fractionDigits: 2,
+  decimalSeparator: ",",
+  thousandsSeparator: ".",
+  cursor: "end",
+};
 
-  input.addEventListener("blur", (evento) => {
-    valida(evento.target)
-  })
-})
+// Select the element
+const input = SimpleMaskMoney.setMask("#txtPreco", args);
+// Convert the input value to a number, which you can save e.g. to a database:
+input.formatToNumber();
+
+/* Realizando a exclusão do produto. */
+
+function deletarProduto() {
+  const elementoProduto = document.querySelector("#btnDelete");
+  const produtoIcons = elementoProduto.parentElement;
+  const produtoDiv = produtoIcons.parentElement;
+  const produtoCompleto = produtoDiv.parentElement;
+  produtoCompleto.remove()
+}
